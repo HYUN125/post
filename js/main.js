95ba7f6ded06aa5765ceac9d740dfba5f9a76bf2
HYUN125 <dodenehxhfl@naver.com>
2023-05-15 16:45:29 +0900

    우체국

--- js/main.js
+++ js/main.js
@@ -21,4 +21,23 @@ $(document).ready(function(){
         },
         slidesPerView : 6,
       });
+
+      // tab
+      $('ul.tabs li').click(function(){
+        let tab_id = $(this).attr('data-tab');
+    
+        $('ul.tabs li').removeClass('current');
+        $('.tab-content').removeClass('current');
+    
+        $(this).addClass('current');
+        $("#"+tab_id).addClass('current');
+      })
+
+      // allmenu-popup
+      const allBtn = $('header .all_menu')
+      const allWrap = $('header .all_menu_popup')
+      allBtn.click(function(){
+        // allWrap.slideToggle(1000);
+        allWrap.slideToggle(600);
+      })
 })
